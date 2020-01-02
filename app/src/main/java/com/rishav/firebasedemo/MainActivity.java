3e9d45183fdd00e657baf990bfe635838fef4393
rishavk1102 <rishavk1102@gmail.com>
2020-01-02 21:50:06 +0530

    Adding Values to RealtimeDatabase

--- app/src/main/java/com/rishav/firebasedemo/MainActivity.java
+++ app/src/main/java/com/rishav/firebasedemo/MainActivity.java
@@ -6,13 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button logout;
+    private EditText edit;
+    private Button add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         logout = findViewById(R.id.logout);
+        edit = findViewById(R.id.edit);
+        add = findViewById(R.id.add);
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +35,18 @@ public class MainActivity extends AppCompatActivity {
                 FirebaseAuth.getInstance().signOut();
                 Toast.makeText(MainActivity.this, "Logged Out!", Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(MainActivity.this , StartActivity.class));
+            }
+        });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_name = edit.getText().toString();
+                if (txt_name.isEmpty()){
+                    Toast.makeText(MainActivity.this, "No name entered!", Toast.LENGTH_SHORT).show();
+                } else {
+                    FirebaseDatabase.getInstance().getReference().child("ProgrammingKnowledge").push().child("Name").setValue(txt_name);
+                }
             }
         });
     }
