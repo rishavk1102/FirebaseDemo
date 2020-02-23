76446ee7b0d56f194a39eb5a267361a21cae1b17
rishavk1102 <rishavk1102@gmail.com>
2020-02-23 17:10:55 +0530

    UserAdapter

---
+++ app/src/main/java/com/rishav/firebasedemo/Adapter/UserAdapter.java
@@ -0,0 +1,113 @@
+package com.rishav.firebasedemo.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.rishav.firebasedemo.Model.User;
+import com.rishav.firebasedemo.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
+
+    private Context mContext;
+    private List<User> mUsers;
+    private boolean isFargment;
+
+    private FirebaseUser firebaseUser;
+
+    public UserAdapter(Context mContext, List<User> mUsers, boolean isFargment) {
+        this.mContext = mContext;
+        this.mUsers = mUsers;
+        this.isFargment = isFargment;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.user_item , parent , false);
+        return new UserAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        User user = mUsers.get(position);
+        holder.btnFollow.setVisibility(View.VISIBLE);
+
+        holder.username.setText(user.getUsername());
+        holder.fullname.setText(user.getName());
+
+        Picasso.get().load(user.getImageurl()).placeholder(R.mipmap.ic_launcher).into(holder.imageProfile);
+
+        isFollowed(user.getId() , holder.btnFollow);
+
+        if (user.getId().equals(firebaseUser.getUid())){
+            holder.btnFollow.setVisibility(View.GONE);
+        }
+
+    }
+
+    private void isFollowed(final String id, final Button btnFollow) {
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Follow").child(firebaseUser.getUid())
+                .child("following");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.child(id).exists())
+                    btnFollow.setText("following");
+                else
+                    btnFollow.setText("follow");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUsers.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public CircleImageView imageProfile;
+        public TextView username;
+        public TextView fullname;
+        public Button btnFollow;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageProfile = itemView.findViewById(R.id.image_profile);
+            username = itemView.findViewById(R.id.username);
+            fullname = itemView.findViewById(R.id.fullname);
+            btnFollow = itemView.findViewById(R.id.btn_follow);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/rishav/firebasedemo/Model/User.java
@@ -0,0 +1,71 @@
+package com.rishav.firebasedemo.Model;
+
+public class User {
+
+    private String name;
+    private String email;
+    private String username;
+    private String bio;
+    private String imageurl;
+    private String id;
+
+    public User() {
+    }
+
+    public User(String name, String email, String username, String bio, String imageurl, String id) {
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.bio = bio;
+        this.imageurl = imageurl;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getImageurl() {
+        return imageurl;
+    }
+
+    public void setImageurl(String imageurl) {
+        this.imageurl = imageurl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
