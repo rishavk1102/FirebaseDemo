e5d74c138b636311ada7ae2ff3e3cc11d6efe098
rishavk1102 <rishavk1102@gmail.com>
2020-02-23 17:27:37 +0530

    Searchfragment java code

--- app/src/main/java/com/rishav/firebasedemo/Fragments/SearchFragment.java
+++ app/src/main/java/com/rishav/firebasedemo/Fragments/SearchFragment.java
@@ -2,20 +2,79 @@ package com.rishav.firebasedemo.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.hendraanggrian.appcompat.widget.SocialAutoCompleteTextView;
+import com.rishav.firebasedemo.Adapter.UserAdapter;
+import com.rishav.firebasedemo.Model.User;
 import com.rishav.firebasedemo.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private List<User> mUsers;
+    private UserAdapter userAdapter;
+
+    private SocialAutoCompleteTextView search_bar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        recyclerView = view.findViewById(R.id.recycler_view_users);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        mUsers = new ArrayList<>();
+        userAdapter = new UserAdapter(getContext() , mUsers , true);
+        recyclerView.setAdapter(userAdapter);
+
+        search_bar = view.findViewById(R.id.search_bar);
+
+        readUsers();
+
+        return view;
+    }
+
+    private void readUsers() {
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Users");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (TextUtils.isEmpty(search_bar.getText().toString())){
+                    mUsers.clear();
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                        User user = snapshot.getValue(User.class);
+                        mUsers.add(user);
+                    }
+
+                    userAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
