eb62b9be7fa2b7e54c6b210a7179492a5bbc48b8
rishavk1102 <rishavk1102@gmail.com>
2020-04-23 04:04:17 +0530

    User Profile UI and Backend

--- app/src/main/java/com/rishav/firebasedemo/Fragments/ProfileFragment.java
+++ app/src/main/java/com/rishav/firebasedemo/Fragments/ProfileFragment.java
@@ -4,20 +4,145 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.rishav.firebasedemo.Model.Post;
+import com.rishav.firebasedemo.Model.User;
 import com.rishav.firebasedemo.R;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileFragment extends Fragment {
+
+    private CircleImageView imageProfile;
+    private ImageView options;
+    private TextView followers;
+    private TextView following;
+    private TextView posts;
+    private TextView fullname;
+    private TextView bio;
+    private TextView username;
+
+    private ImageView myPictures;
+    private ImageView savedPictures;
+
+    private FirebaseUser fUser;
+
+    String profileId;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        fUser = FirebaseAuth.getInstance().getCurrentUser();
+        profileId = fUser.getUid();
+
+        imageProfile = view.findViewById(R.id.image_profile);
+        options = view.findViewById(R.id.options);
+        followers = view.findViewById(R.id.followers);
+        following = view.findViewById(R.id.following);
+        posts = view.findViewById(R.id.posts);
+        fullname = view.findViewById(R.id.fullname);
+        bio = view.findViewById(R.id.bio);
+        username = view.findViewById(R.id.username);
+        myPictures = view.findViewById(R.id.my_pictures);
+        savedPictures = view.findViewById(R.id.saved_pictures);
+
+        userInfo();
+        getFollowersAndFollowingCount();
+        getPostCount();
+
+        return view;
+    }
+
+    private void getPostCount() {
+
+        FirebaseDatabase.getInstance().getReference().child("Posts").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                int counter = 0;
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Post post = snapshot.getValue(Post.class);
+
+                    if (post.getPublisher().equals(profileId)) counter ++;
+                }
+
+                posts.setText(String.valueOf(counter));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void getFollowersAndFollowingCount() {
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Follow").child(profileId);
+
+        ref.child("followers").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                followers.setText("" + dataSnapshot.getChildrenCount());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        ref.child("following").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                following.setText("" + dataSnapshot.getChildrenCount());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void userInfo() {
+
+        FirebaseDatabase.getInstance().getReference().child("Users").child(profileId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+
+                Picasso.get().load(user.getImageurl()).into(imageProfile);
+                username.setText(user.getUsername());
+                fullname.setText(user.getName());
+                bio.setText(user.getBio());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
