bf576b8e92a4b4a87ae5a0cbdd166dd83937499e
rishavk1102 <rishavk1102@gmail.com>
2020-04-30 01:54:17 +0530

    Update Comments and redirectiong to profile fragment for viewing other userd profile.

---
+++ app/src/main/java/com/rishav/firebasedemo/Adapter/PhotoAdapter.java
@@ -0,0 +1,59 @@
+package com.rishav.firebasedemo.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.rishav.firebasedemo.Model.Post;
+import com.rishav.firebasedemo.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class PhotoAdapter extends RecyclerView.Adapter<PhotoAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List<Post> mPosts;
+
+    public PhotoAdapter(Context mContext, List<Post> mPosts) {
+        this.mContext = mContext;
+        this.mPosts = mPosts;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.photo_item, parent, false);
+        return  new PhotoAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        Post post = mPosts.get(position);
+        Picasso.get().load(post.getImageurl()).placeholder(R.mipmap.ic_launcher).into(holder.postImage);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPosts.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ImageView postImage;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            postImage = itemView.findViewById(R.id.post_image);
+        }
+    }
+
+}
--- app/src/main/java/com/rishav/firebasedemo/Fragments/ProfileFragment.java
+++ app/src/main/java/com/rishav/firebasedemo/Fragments/ProfileFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,14 +24,23 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.rishav.firebasedemo.Adapter.PhotoAdapter;
 import com.rishav.firebasedemo.Model.Post;
 import com.rishav.firebasedemo.Model.User;
 import com.rishav.firebasedemo.R;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private PhotoAdapter photoAdapter;
+    private List<Post> myPhotoList;
 
     private CircleImageView imageProfile;
     private ImageView options;
@@ -76,9 +87,17 @@ public class ProfileFragment extends Fragment {
         savedPictures = view.findViewById(R.id.saved_pictures);
         editProfile = view.findViewById(R.id.edit_profile);
 
+        recyclerView = view.findViewById(R.id.recucler_view_pictures);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
+        myPhotoList = new ArrayList<>();
+        photoAdapter = new PhotoAdapter(getContext(), myPhotoList);
+        recyclerView.setAdapter(photoAdapter);
+
         userInfo();
         getFollowersAndFollowingCount();
         getPostCount();
+        myPhotos();
 
         if (profileId.equals(fUser.getUid())) {
             editProfile.setText("Edit profile");
@@ -112,6 +131,32 @@ public class ProfileFragment extends Fragment {
         });
 
         return view;
+    }
+
+    private void myPhotos() {
+
+        FirebaseDatabase.getInstance().getReference().child("Posts").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                myPhotoList.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Post post = snapshot.getValue(Post.class);
+
+                    if (post.getPublisher().equals(profileId)) {
+                        myPhotoList.add(post);
+                    }
+                }
+
+                Collections.reverse(myPhotoList);
+                photoAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
     private void checkFollowingStatus() {
